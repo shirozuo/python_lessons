b86aaec633c8c244799c8849fa2781d9e7f0d3fa
Roma <shiroyonago@gmail.com>
2024-01-30 17:43:48 +0300

    Initial commit

---
+++ solution 162/main.py
@@ -0,0 +1,12 @@
+class ExtendedStack(list):
+    def sum(self):
+        # операция сложения
+
+    def sub(self):
+        # операция вычитания
+
+    def mul(self):
+        # операция умножения
+
+    def div(self):
+        # операция целочисленного деления
