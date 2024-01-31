49d0a9ef91de16a6261045570d6e4dde3d92d2a8
Roman <shiroyonago@gmail.com>
2024-01-31 04:07:55 +0300

    Fill function's code Task completed

--- solution 162/main.py
+++ solution 162/main.py
@@ -1,12 +1,17 @@
 class ExtendedStack(list):
     def sum(self):
         # операция сложения
+        self.append(self.pop() + self.pop())
+
 
     def sub(self):
         # операция вычитания
+        self.append(self.pop() - self.pop())
 
     def mul(self):
         # операция умножения
+        self.append(self.pop() * self.pop())
 
     def div(self):
         # операция целочисленного деления
+        self.append(self.pop() // self.pop())
