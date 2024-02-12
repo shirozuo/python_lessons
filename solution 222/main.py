f57c243741a0c1e7258d0a378bee5943267212b9
Roman <shiroyonago@gmail.com>
2024-02-12 05:35:54 +0300

    Task complete

--- solution 222/main.py
+++ solution 222/main.py
@@ -1,4 +1,5 @@
 import urllib.request
+from simplecrypt import decrypt, DecryptionException
 
 _encrypted_url = "https://stepik.org/media/attachments/lesson/24466/encrypted.bin"
 _passwords_url = "https://stepik.org/media/attachments/lesson/24466/passwords.txt"
@@ -6,5 +7,15 @@ _passwords_url = "https://stepik.org/media/attachments/lesson/24466/passwords.tx
 encrypted = urllib.request.urlopen(_encrypted_url).read()
 passwords = urllib.request.urlopen(_passwords_url).read().strip().split()
 
-print(encrypted)
-print(passwords)
+decrypted_list = list()
+for password in passwords:
+    print("Trying password: ", password)
+    try:
+        message = decrypt(password, encrypted).decode("utf-8")
+        decrypted_list.append(message)
+        print("VALID PASSWORD:", password, "DECRYPTED MESSAGE:", message)
+    except DecryptionException as e:
+        print("Found error: ", e)
+
+print("\n" + "All decrypted messages:")
+print("\n".join(decrypted_list))
